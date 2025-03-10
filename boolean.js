23421f46dfa4bc0b025dd453073f5bf227727e41
Gide3 <162450627+Gide3@users.noreply.github.com>
2025-03-10 08:33:40 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,17 @@
+// Operadores de Comparação
+// Operador de valor (compara apenas o valor)
+console.log("1" == 1)
+console.log(true == 1)
+console.log(false == 0)
+console.log(false == "")
+
+// Operadores de igualdade estrita
+// (compara valor e tipo)
+console.log("1" === 1)
+console.log(true === 1)
+console.log(false === 0)
+
+// Verificar tipos de variável
+console.log(typeof("1")) // -> texto (string)
+console.log(typeof(1)) // -> number
+console.log(typeof (true)) //-> boolean
---
+++ exercicio1.js
@@ -0,0 +1,19 @@
+let peso = 75
+let agua = 35
+
+let qtd_agua_ml = peso * 35
+
+let agua_lt = qtd_agua_ml / 1000
+
+console.log("Quantidade de agua = " + agua_lt + " Litros");
+
+
+const av1 = 10.0
+const av2 = 7.0
+const av3 = 4.0
+const media =(av1+av2+av3)/3
+if(media>=6.0){
+    console.log("Aprovado com média 7.0");
+}else {
+    console.log("Reprovado");
+}
