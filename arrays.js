ec4cb400e35c5baeb5298d201437cbae738cfcaf
Gide3 <162450627+Gide3@users.noreply.github.com>
2025-03-11 11:58:52 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// LISTAS
+// lista de String:
+const lanches = ["x-egg", "x-ovo", "x-bacon", "x-salada", "x-frango"];
+// lista de Number:
+const precos = [12, 15, 15, 20, 12];
+console.log(lanches)
+console.log(precos)
+//acessar um item especifico da lista:
+console.log(lanches [2] + "R$" + precos [2])
+//x-egg
+
+const tamanho = lanches.length
+console.log(tamanho)
