f5e21cd2e63592968b9b897ae092e61f20cd96ee
Gide3 <162450627+Gide3@users.noreply.github.com>
2025-04-28 08:31:59 -0300

    Add files via upload

--- times.js
+++ times.js
@@ -1,90 +1,43 @@
-let times= [
-    {
-        Nome:"Magnus Futsal",
-        Goleiro:"André Deko",
-        Fivô:"Rogrigo capita",
-        AlaDireira:"Gayraud",
-        AlaEsquerda:"Leandro Lino",
-        Pivô:"Leozin",
-        Tecnico:"Douglão",
-        Vitória:31,
-        Derrotas:5,
-    },
-    {
-        Nome:"Jaraguá",
-        Goleiro:"Andreson",
-        Fivô:"Nenem",
-        AlaDireira:"Yan",
-        AlaEsquerda:"Santiago",
-        Pivô:"Eka",
-        Tecnico:"Xande Melo",
-        Vitória:31,
-        Derrotas:4,
-    },
-    {
-        Nome:"Sport Club Corinthians Paulista",
-        Goleiro:"Koji",
-        Fivô:"Bancatelli",
-        AlaDireira:"Igor carioca",
-        AlaEsquerda:"Levy",
-        Pivô:"Andrey",
-        Tecnico:"Malafaia",
-        Vitória:13,
-        Derrotas:15,
-    },
-    {
-        Nome:"Joninville Futsal",
-        Goleiro:"Kleyton",
-        Fivô:"Ernani",
-        AlaDireira:"Xuxa",
-        AlaEsquerda:"Pedro Rei",
-        Pivô:"Rufino",
-        Tecnico:"Herick",
-        Vitória:31,
-        Derrotas:4,
-    },
-    {
-        Nome:"Epa Futsal",
-        Goleiro:"Dudu",
-        Fivô:"Xandy",
-        AlaDireira:"Luiz Gustavo",
-        AlaEsquerda:"André",
-        Pivô:"Gide",
-        Tecnico:"Nelson",
-        Vitória:2,
-        Derrotas:1,
-    }  
-]
+let times=require("./dados.json")
 
-function vertimes () {
-    times. forEach((time) => {
-           console.log (time.nome + "-" + time.goleiro + "(goleiro)", time.aladireita + "(ala)" , time.alaesquerda + "(ala)" + time.pivô + "(pivô)" + time.fixo + "(fixo)" + time.técnico + time.vitorias + time.derrotas)
-    } );
+function verTimes(){
+    times.forEach((time)=> {
+        console.log(time.nome + " - jogadores: " + " goleiro - " + time.goleiro + ", Fixo - " + time.fixo + ", Alá Direito - " + time.AlaDireita + ", Alá Esquerdo - " + time.AlaEsquerdo + ", Pivô - " + time.pivo + ", Técnico - " + time.tecnico + ", Vitórias " + time.vitorias + ", Derrotas " + time.derrotas)
+    })
+}
+verTimes()
+
+function criarArquivo(){
+    let dadosEmTexto = JSON.stringify(times);
+    const fs = require("fs");
+    fs.writeFileSync("dados.json", dadosEmTexto);
+    console.log("Arquivo criado");
 }
 
-function vertimes ( ){ 
-    times.forEach ((time) => {
-        console.log(time.nome+ "-" + time.goleiro +"(goleiro),"+
-            time.goleiro + "(goleiro)," +
-            time.alaDireita + "(ala dir.), " +
-
-
-        
-    )}
-}
-
-function marcarjogos(timeA, timeB, data, horario, cidade){
-    let jogos = require("./jogos.json");
-    jogo = {
-        timeA: timeB,
+//criarArquivo()
+function marcarJogos(timeA, timeB, estadio, data, horario){
+    let jogos=require("./jogos.json");
+    jogo ={
+        timeA: timeA,
         timeB: timeB,
+        estadio: estadio,
         data: data,
         horario: horario,
-        esta
     }
-
+    jogos.push(jogo);
+    jogosJSON = JSON.stringify(jogos);
+    const fs=require('fs');
+    fs.writeFileSync("jogos.json", jogosJSON);
 }
 
+marcarJogos("Joiville", "Magnus", "Ginásio Patão", "30/04/2025", "20:30")
 
+//criarArquivo();
 
-   
+function mostrarJogos(){
+    let jogos = require("./jogos.json");
+    jogos.forEach((jogo)=>{
+        console.log(jogo.timeA + " - " + jogo.timeB + " - " + jogo.estadio + " - " + jogo.data + " - " + jogo.horario + " - ")
+    });
+}
+mostrarJogos();
